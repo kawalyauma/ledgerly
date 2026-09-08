235d6429dfd3971fa87da4c114d88b0a0234bc5b
kawalyauma <kawalyaumar5000@gmail.com>
2026-09-09 01:31:05 +0300

    Build complete Ledgerly Mobile Human Resources module

--- modules/human-resources/backend/module.ts
+++ modules/human-resources/backend/module.ts
@@ -1,4 +1,18 @@
 import type { BackendModuleDefinition } from "../../backend-types";
 import { humanResourcesRoutes } from "./routes";
+import { humanResourcesOnboardingWorkflowRoutes } from "./onboarding-workflow";
+import { humanResourcesAdminWorkflowRoutes } from "./admin-workflow";
+import { humanResourcesAuditWorkflowRoutes } from "./audit-workflow";
 
-export const moduleDefinition: BackendModuleDefinition = { key:"human-resources",name:"Human Resources",version:"1.0.0",order:22,routes:[{basePath:"/api/v1/human-resources",router:humanResourcesRoutes}] };
+export const moduleDefinition: BackendModuleDefinition = {
+  key:"human-resources",
+  name:"Human Resources",
+  version:"1.0.0",
+  order:22,
+  routes:[
+    {basePath:"/api/v1/human-resources",router:humanResourcesRoutes},
+    {basePath:"/api/v1/human-resources",router:humanResourcesOnboardingWorkflowRoutes},
+    {basePath:"/api/v1/human-resources",router:humanResourcesAdminWorkflowRoutes},
+    {basePath:"/api/v1/human-resources",router:humanResourcesAuditWorkflowRoutes},
+  ],
+};
