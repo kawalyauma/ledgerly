0d0653e5503397219b441683159097c02662a854
kawalyauma <kawalyaumar5000@gmail.com>
2026-09-09 02:27:53 +0300

    Add offline Contacts mobile sync

--- modules/contacts/backend/module.ts
+++ modules/contacts/backend/module.ts
@@ -1,3 +1,4 @@
+import "./mobile-sync";
 import type { BackendModuleDefinition } from "../../backend-types";
 import { contactsRoutes } from "./routes";
 import { contactsManagementRoutes } from "./management-workflow";
@@ -5,7 +6,7 @@ import { contactsManagementRoutes } from "./management-workflow";
 export const moduleDefinition: BackendModuleDefinition = {
   key: "contacts",
   name: "Contacts",
-  version: "1.0.0",
+  version: "1.1.0",
   order: 15,
   routes: [
     { basePath: "/api/v1/contacts", router: contactsRoutes },
