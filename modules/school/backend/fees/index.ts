31c275d8be43f1601fa72d9e6f6ffe2c4b126704
kawalyauma <kawalyaumar5000@gmail.com>
2026-09-09 00:38:50 +0300

    Add offline School Fees mobile sync

--- modules/school/backend/fees/index.ts
+++ modules/school/backend/fees/index.ts
@@ -5,11 +5,7 @@ import { schoolFeeBillingRoutes } from "./billing";
 import { schoolFeePaymentRoutes } from "./payments";
 import { schoolFeeReportRoutes } from "./reports";
 import { schoolFeeReceiptPrintRoutes } from "./receipt-printing";
-
+import { schoolFeeMobileIntentRoutes } from "./mobile-intents";
 /** School Fees & Billing subledger over Ledgerly AR, payments, journals and contacts. */
 export const schoolFeesRoutes=new Hono<{Bindings:Env;Variables:AppVariables}>();
-schoolFeesRoutes.route("/",schoolFeeStructureRoutes);
-schoolFeesRoutes.route("/",schoolFeeBillingRoutes);
-schoolFeesRoutes.route("/",schoolFeePaymentRoutes);
-schoolFeesRoutes.route("/",schoolFeeReportRoutes);
-schoolFeesRoutes.route("/",schoolFeeReceiptPrintRoutes);
+schoolFeesRoutes.route("/",schoolFeeStructureRoutes);schoolFeesRoutes.route("/",schoolFeeBillingRoutes);schoolFeesRoutes.route("/",schoolFeePaymentRoutes);schoolFeesRoutes.route("/",schoolFeeReportRoutes);schoolFeesRoutes.route("/",schoolFeeReceiptPrintRoutes);schoolFeesRoutes.route("/",schoolFeeMobileIntentRoutes);
